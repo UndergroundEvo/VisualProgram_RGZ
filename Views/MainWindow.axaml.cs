6824d1d39a84813e7b74d9dedd14454bf6f9429d
Miron <popovmiron@gmail.com>
2023-05-28 22:21:39 +0700

    Добавьте файлы проекта.

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace RGZ.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
