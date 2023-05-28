6f13d19215b000e57d2e1ac0c81b74ff2e1b126e
Miron <popovmiron@gmail.com>
2023-05-28 22:49:11 +0700

    Added LogicSimulator & UITests

---
+++ LogicSimulator/Models/JoinedItems.cs
@@ -0,0 +1,30 @@
+﻿using Avalonia.Controls.Shapes;
+using Avalonia.Media;
+using System.Collections.Generic;
+
+namespace LogicSimulator.Models {
+    public class JoinedItems {
+        public Line line = new() { Tag = "Join", ZIndex = 2, Stroke = Brushes.DarkGray, StrokeThickness = 3 };
+        public static readonly Dictionary<Line, JoinedItems> arrow_to_join = new(); // объект для связывания двух элементов линией
+        public void Update()
+        {
+            line.StartPoint = A.GetPos();
+            line.EndPoint = B.GetPos();
+        }
+        public void Delete()
+        {
+            arrow_to_join.Remove(line);
+            line.Remove();
+            A.parent.RemoveJoin(this);
+            B.parent.RemoveJoin(this);
+        }
+        public Distantor A { get; set; }
+        public Distantor B { get; set; }
+        public JoinedItems(Distantor a, Distantor b) {  
+            A = a; B = b; Update(); // устанавливает начало и конец линии на позиции элементов A и B
+            a.parent.AddJoin(this); 
+            if (a.parent != b.parent) b.parent.AddJoin(this);
+            arrow_to_join[line] = this;
+        }
+    }
+}
---
+++ LogicSimulator/Views/Shapes/Switch.axaml.cs
@@ -0,0 +1,74 @@
+using Avalonia;
+using Avalonia.Input;
+using Avalonia.Media;
+using Avalonia.Controls;
+using LogicSimulator.Models;
+using LogicSimulator.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace LogicSimulator.Views.Shapes
+{
+    public partial class Switch : GateBase, IGate, INotifyPropertyChanged
+    {
+        public override int TypeId => 5;
+
+        public override UserControl GetSelf() => this;
+        protected override IGate GetSelfI => this;
+        protected override int[][] Sides => new int[][] {
+            Array.Empty<int>(),
+            Array.Empty<int>(),
+            new int[] { 1 },
+            Array.Empty<int>()
+        };
+
+        protected override void Init() => InitializeComponent();
+
+        /*
+         * Мозги
+         */
+
+        bool my_state = false;
+        Point? press_pos;
+
+        private static Point GetPos(PointerEventArgs e)
+        {
+            if (e.Source is not Control src) return new();
+            while ((string?)src.Tag != "scene" && src.Parent != null) src = (Control)src.Parent;
+            return e.GetCurrentPoint(src).Position;
+        }
+        private void Press(object? sender, PointerPressedEventArgs e)
+        {
+            if (e.Source == border) press_pos = GetPos(e);
+        }
+        private void Release(object? sender, PointerReleasedEventArgs e)
+        {
+            if (e.Source != border) return;
+            if (press_pos == null || GetPos(e).Hypot((Point)press_pos) > 5) return;
+            press_pos = null;
+
+            my_state = !my_state;
+            border.Background = new SolidColorBrush(Color.Parse(my_state ? "#8F3535" : "#C15757"));
+        }
+
+        public void Brain(ref bool[] ins, ref bool[] outs) => outs[0] = my_state;
+
+
+        public override Dictionary<string, object> ExtraExport() => new() { ["state"] = my_state };
+
+        public override void ExtraImport(string key, object extra)
+        {
+            if (key != "state") { Log.Write(key + "-запись элемента не поддерживается"); return; }
+            if (extra is not bool @state) { Log.Write("Неверный тип state-записи элемента: " + extra); return; }
+            my_state = @state;
+            if (my_state) border.Background = new SolidColorBrush(Color.Parse("#8F3535"));
+        }
+
+        public void SetState(bool state)
+        {
+            my_state = state;
+            border.Background = new SolidColorBrush(Color.Parse(state ? "#8F3535" : "#C15757"));
+        }
+    }
+}
